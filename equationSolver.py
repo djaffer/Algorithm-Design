95163196fe066b89d0afac61659edc943ea71fc5
Danish J <danish.jaffer@utexas.edu>
2019-05-07 20:32:50 -0700

    updated with two different ways to solve equation

--- equationSolver.py
+++ equationSolver.py
@@ -1,99 +1,128 @@
 import random
-def evaluate(exp):
-  exp = exp.replace(' ','')
-  vals = []
-  v = ""
-  operators = ['+','-','/','*']
-  for i in range(len(exp)):
-    n = exp[i]
-    if n in operators:
-      vals.append(v)
-      vals.append(n)
-      v = ""
+def evaluate2pass(exp):
+  operator = []
+  operand = []
+  ops = ['+','-','/','*']
+  i = 0
+  n = len(exp)
+  op = 0
+  prev = 0
+  while i < n:
+    ch = exp[i]
+    
+    if ch == ' ' or ch =='\t':
+      i += 1
+      continue
+    
+    if ch in ops:
+      op = ch
+      operator.append(ch)
+      i += 1
     else:
-      v += exp[i]
-  
-  vals.append(v)
-  while "*" in vals or "/" in vals:
-    
-    res = pimdas(vals,"*/")
-    i = 0
-    a = ""
-    b = "" 
-    c = ""
-    if res["*"]:
-      i = res["*"]
-      b = vals.pop(i)
-      a = vals.pop(i-1)
-      c = vals.pop(i-1)
-      
-    elif res["/"]:
-      i = res["/"]
-      b = vals.pop(i)
-      a = vals.pop(i-1)
-      c = vals.pop(i-1)
-      
-    
-    vals.insert(i-1,calculate(a,b,c))
-    
-  while "+" in vals or "-" in vals:
-    
-    res = pimdas(vals,"+-")
-    i = 0
-    a = ""
-    b = "" 
-    c = ""
+      num, i = getNumber(exp,i)
+      operand.append(num)
 
-    if res["+"]:
-      i = res["+"]
-      b = vals.pop(i)
-      a = vals.pop(i-1)
-      c = vals.pop(i-1)
-    elif res["-"]:
-      i = res["-"]
-      b = vals.pop(i)
-      a = vals.pop(i-1)
-      c = vals.pop(i-1)
-    vals.insert(i-1,calculate(a,b,c))
-  return float(vals.pop())
-def pimdas(arr,oper):
-  count = 0
-  res = {"+":None,"-":None,"/":None,"*":None}
-  for i in range(len(arr)):
-    
-    if not res["*"] and arr[i] == "*":
-      res["*"] = i
-      count += 1
-    elif not res["/"] and arr[i] == "/":
-      res["/"] = i
-      count += 1
-    elif not res["+"] and arr[i] == "+":
-      res["+"] = i
-      count += 1
-    elif not res["-"] and arr[i] == "-":
-      res["-"] = i
-      count += 1
-      
-    if oper=="*/" and (res['*'] or res['/']):
-      break
-    
-    if oper=="+-" and (res['+'] or res['-']):
-      break
+      if op in ['*','/']:
+        oper = operator.pop()
+        y = operand.pop()
+        x = operand.pop()
+        operand.append(calculate(x,y,oper))
+  
+  if len(operand) == 1:
+    return operand.pop()
+  res = operand[0]
+
+  j = 1
+  
+  for oper in operator:
+    res = calculate(res,operand[j],oper)
+    j += 1
+
+
+ 
   return res
 
 
-def calculate(x,y,z):
-  x = float(x)
-  z = float(z)
-  if y == "+":
-    return x + z
-  elif y == "-":
-    return x - z
-  elif y == "/":
-    return x / z
-  elif y == "*":
-    return x * z
-  return -1
+def calculate(x,y,oper):
+
+  if oper == "*":
+    return x * y
+  elif oper == '/':
+    return x / y
+  elif oper == '+':
+    return x + y
+  elif oper == '-':
+    return x - y
+
+
+
+def getNumber(exp,start):
+  num = ""
+  while start<len(exp):
+    ch = exp[start]
+    if exp[start] ==' ' or exp[start] == '/t':
+      start += 1
+      continue
+    if (ch >= '0' and ch <='9') or ch==".":
+      num += ch
+    else:
+      break
+    start += 1
+  
+  return float(num),start
+
+
+def evaluatePostfix(exp):
+  postfix_eq= []
+  operators = ['+','-','/','*']
+  ops = []
+  i = 0
+  n = len(exp)
+  while i < n:
+    if exp[i] ==' ' or exp[i] == '/t':
+      i += 1
+      continue
+    if (exp[i] in operators):
+      while ops and preced(ops[-1],exp[i]):
+        postfix_eq.append(ops.pop())
+      ops.append(exp[i])
+      op = exp[i]
+      i += 1
+    else:
+      num,i = getNumber(exp,i)
+      postfix_eq.append(num)
+
+  while ops:
+    postfix_eq.append(ops.pop())
+  
+  return postfixCalculate(postfix_eq)
+
+def preced(op1, op2):
+  if op1 == '*' or op1 == '/':
+    return True
+
+  if op1 == '+' or op1 == '-':
+    if op2 == '+' or op2 == '-':
+      return True
+
+  return False
+
+def postfixCalculate(exp):
+  stack = []
+  
+  for e in exp:
+
+    if e in ['+','-','/','*']:
+      y = stack.pop()
+      x = stack.pop()
+      
+      # print(x," ",y," ",e)
+      res = calculate(float(x),float(y),e)
+      stack.append(res)
+    else:
+      stack.append(e)
+  return stack.pop()
+
 def generate_expression(nOperators):
   ops = ['+', '/', '*', '-']
   d = random.random() * 100
@@ -112,14 +141,16 @@ def generate_expression(nOperators):
 
 def test(expression):
   result_lib = eval(expression)
-  result = evaluate(expression)
-  print(result_lib," ",result)
+  result = evaluate2pass(expression)
+  result_postfix = evaluatePostfix(expression)
+  print("Correct Answer ", result_lib," 2 Pass:",result,"Post Fix",result_postfix)
   assert result_lib == result
+  assert result_lib == result_postfix
 
 
 
 test("5*2/2")
+
 for i in xrange(1, 3000, 1000):
   expression = generate_expression(i)
   test(expression)
-
